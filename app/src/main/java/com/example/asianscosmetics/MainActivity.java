1de31e953095df4d5dc1c441d57997bee98040c0
Runfeng Gan <patrickgan1355@gmail.com>
2020-07-09 20:09:37 -0400

    i have done my part

--- app/src/main/java/com/example/asianscosmetics/MainActivity.java
+++ app/src/main/java/com/example/asianscosmetics/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.asianscosmetics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button  = (Button) findViewById(R.id.welcome_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,LoginAcitivity.class));
+            }
+        });
     }
 }
