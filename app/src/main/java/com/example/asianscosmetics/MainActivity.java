7b2032c0d22f37790fe75745ef051c994b30e882
Runfeng Gan <patrickgan1355@gmail.com>
2020-07-09 23:37:22 -0400

    change logo background cant resolve problem with launching second activity

--- app/src/main/java/com/example/asianscosmetics/MainActivity.java
+++ app/src/main/java/com/example/asianscosmetics/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,LoginAcitivity.class));
+                startActivity(new Intent(MainActivity.this,Login_Activity.class));
             }
         });
     }
