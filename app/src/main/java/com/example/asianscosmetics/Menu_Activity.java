4828bcc72770dfc7695bc31e649598cb084c552e
sandeep-thind <seemu34@gmail.com>
2020-07-11 16:04:32 -0400

    ..

--- app/src/main/java/com/example/asianscosmetics/Menu_Activity.java
+++ app/src/main/java/com/example/asianscosmetics/Menu_Activity.java
@@ -10,6 +10,5 @@ public class Menu_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_);
-
     }
 }
