95ddefc08562e3dba9b5c23261376968e81a20d0
sandeep-thind <seemu34@gmail.com>
2020-07-12 09:34:11 -0400

    Added cart and menu activity(empty)

---
+++ app/src/main/java/com/example/asianscosmetics/Cart_Activity.java
@@ -0,0 +1,15 @@
+package com.example.asianscosmetics;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Cart_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_);
+
+    }
+}
--- app/src/main/java/com/example/asianscosmetics/Menu_Activity.java
+++ app/src/main/java/com/example/asianscosmetics/Menu_Activity.java
@@ -1,14 +1,24 @@
 package com.example.asianscosmetics;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Menu_Activity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_);
+        button = findViewById(R.id.cart_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Menu_Activity.this, Cart_Activity.class));
+            }
+        });
     }
 }
