e8442fc2539394bffe442aef505dce9190acb3ab
Ru-Max <61360988+Ru-Max@users.noreply.github.com>
2020-09-08 19:03:36 -0400

    I connect to the database

--- app/src/main/java/com/example/asianscosmetics/Signup_Activity.java
+++ app/src/main/java/com/example/asianscosmetics/Signup_Activity.java
@@ -2,25 +2,85 @@ package com.example.asianscosmetics;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
+
 public class Signup_Activity extends AppCompatActivity {
-    private Button button;
+    Button button;
+    EditText mFullName, mUsername, mEmail, mPassword, mPasswordConfirm;
+    ProgressBar progressBar;
+    TextView mRegister;
+    FirebaseAuth fAuth;
+
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_);
 
         button = findViewById(R.id.login_button);
+        mFullName = findViewById(R.id.FullName);
+        mUsername = findViewById(R.id.UserName);
+        mEmail = findViewById(R.id.Email);
+        mPassword = findViewById(R.id.Password);
+        mPasswordConfirm = findViewById(R.id.PasswordConfirm);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+        mRegister = findViewById(R.id.Login);
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Signup_Activity.this, Login_Activity.class));
-            }
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is required.");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is required.");
+                    return;
+                }
+                if(password.length() < 6){
+                    mPassword.setError("Password  must be  >= 6 character.");
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Signup_Activity.this, "User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }else{
+                            Toast.makeText(Signup_Activity.this, "Error !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            };
         });
-    }
-}
+}}
